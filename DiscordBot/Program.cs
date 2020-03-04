0e51990619d560288a362f4115ed66ecac3a7964
Léo DUBOIN <leo@d4ilyrun.localdomain>
2020-03-04 18:16:30 +0100

    push project structure

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+           _client = new DiscordSocketClient(); // Connection au client
+           _client.Log += Log;
+
+           // Temporary => Unsecure
+           string token = "j3M-wRMavvK049-TbNz8XxTAtfugdXJn";
+
+           await _client.LoginAsync(TokenType.Bot, token);
+           //Blocks the task until the program is closed
+           await (Task.Delay(-1));
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
